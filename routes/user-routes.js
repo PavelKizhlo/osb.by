a8e185de03f8a67234973b35423d111f6e8a47c0
Pavel Kizhlo <pavelkizh@gmail.com>
2022-12-26 15:12:44 +0300

    feat: implement cards rendering from db data

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const createPath = require('../helpers/create-path');
+const db = require('../db/db');
 
 const router = express.Router();
 
-router.get(['/', '/home', '/index'], (req, res) => {
-  res.render(createPath('index.ejs'));
+router.get(['/', '/home', '/index'], async (req, res) => {
+  try {
+    const cards = await db.query('SELECT * FROM card WHERE is_popular = 1');
+    const slides = await db
+      .query('SELECT * FROM slider')
+      .then((res) => res.map((item) => item.img_src));
+    res.render(createPath('index.ejs'), { cards, slides });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.toString() });
+  }
 });
 
-router.get('/catalog', (req, res) => {
-  res.render(createPath('catalog.ejs'));
+router.get('/catalog', async (req, res) => {
+  try {
+    const cards = await db.query('SELECT * FROM card');
+    const thickness = await db
+      .query('SELECT DISTINCT thickness FROM card ORDER BY thickness ASC')
+      .then((res) => res.map((item) => item.thickness));
+    res.render(createPath('catalog.ejs'), { cards, thickness });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.toString() });
+  }
 });
 
 router.get('/about', (req, res) => {
