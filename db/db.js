1faba701d89b4c4588bf39b695789e9bc7ded543
Pavel Kizhlo <pavelkizh@gmail.com>
2023-01-19 16:19:42 +0300

    refactor: refactor db

--- db/db.js
+++ db/db.js
@@ -1,31 +1,42 @@
+require('dotenv').config();
 const chalk = require('chalk');
-const pool = require('./config');
+const mariadb = require('mariadb');
 
 const errorMsg = chalk.bgWhite.redBright;
 const successMsg = chalk.bgWhite.greenBright;
 
 class Database {
   constructor() {
-    this.connection = null;
+    this.pool = null;
   }
 
   async connect() {
     try {
-      this.connection = await pool.getConnection();
+      this.pool = mariadb.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        connectionLimit: 5,
+      });
+
       console.log(successMsg('DB connected'));
     } catch (err) {
       console.log(errorMsg(`DB connection failed! Reason: ${err.message}`));
-    } finally {
-      if (this.connection) await this.connection.release();
     }
   }
 
   async query(sql, args) {
+    let connection;
     try {
-      const result = await this.connection.query(sql, args);
+      connection = await this.pool.getConnection();
+      const result = await connection.query(sql, args);
       return result;
     } catch (err) {
       console.log(errorMsg(err.message));
+    } finally {
+      if (connection) await connection.release();
     }
   }
 }
